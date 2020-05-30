b2eb6c3a852168720cd1c6fc2e0bcd7a2c5f120c
CutThroat <55604778+cutthroat78@users.noreply.github.com>
2020-05-30 22:43:31 +0100

    First Commit

---
+++ Extreme/Ultimate Antivirus Elite Extreme (With tree) (Self Destruct).bat
@@ -0,0 +1,27 @@
+@echo off
+TITLE "Ultimate Antivirus Elite"
+ECHO "Starting..."
+PING 1.0.0.0 > NUL
+TITLE "Scanning..."
+ECHO "Starting Malware/Virus Scan..."
+TREE /
+TREE /
+ECHO "Scan Complete"
+PING 1.0.0.0 > NUL
+ECHO "Checking if IP is compromised"
+PING 1.0.0.0 > NUL
+PING 1.0.0.0 > NUL
+ECHO "Checking if any hackers have infected Computer"
+PING 1.0.0.0 > NUL
+PING 1.0.0.0 > NUL
+PING 1.0.0.0 > NUL
+ECHO "Checking if hackers have infected Network"
+PING 1.0.0.0 > NUL
+PING 1.0.0.0 > NUL
+PING 1.0.0.0 > NUL
+PING 1.0.0.0 > NUL
+TITLE "Scan Done"
+ECHO "%RANDOM% Virus found, Malware installed, IP is compromised, Computer and Network has Hackers"
+PING 1.0.0.0 > NUL
+start /b "" cmd /c del "%~f0"&exit /b
+EXIT
---
+++ Normal/Ultimate Antivirus Elite (Self Destruct).bat
@@ -0,0 +1,24 @@
+@echo off
+TITLE "Ultimate Antivirus Elite"
+ECHO "Starting..."
+PING 1.0.0.0 > NUL
+TITLE "Scanning..."
+ECHO "Starting Malware/Virus Scan..."
+PING 1.0.0.0 > NUL
+ECHO "Scanning Program Files..."
+PING 1.0.0.0 > NUL
+ECHO "Scanning User Files..."
+PING 1.0.0.0 > NUL
+ECHO "Scan Complete"
+PING 1.0.0.0 > NUL
+ECHO "Checking if IP is compromised"
+PING 1.0.0.0 > NUL
+ECHO "Checking if any hackers have infected Computer"
+PING 1.0.0.0 > NUL
+ECHO "Checking if hackers have infected Network"
+PING 1.0.0.0 > NUL
+TITLE "Scan Done"
+ECHO "%RANDOM% Virus found, Malware installed, IP is compromised, Computer and Network has Hackers"
+PING 1.0.0.0 > NUL
+start /b "" cmd /c del "%~f0"&exit /b
+EXIT
